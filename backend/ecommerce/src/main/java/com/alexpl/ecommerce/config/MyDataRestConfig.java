c2a9aeb3311f0bfab3efdb0b0a9814d7ef57ce2b
Alexandros Plessias <67461036+AlexandrosPlessias@users.noreply.github.com>
2021-08-29 15:45:54 +0300

    Add MyDataRestConfig for READ-ONLY support
    The MyDataRestConfig Disable HTTP methods for Product: PUT, POST, DELETE for having only READ-ONLY support. By default Spring Dara REST API allo all CRUD operations, we don;t want this.

---
+++ backend/ecommerce/src/main/java/com/alexpl/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.alexpl.ecommerce.config;
+
+import com.alexpl.ecommerce.entity.Product;
+import com.alexpl.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // The READ-ONLY policy.
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable HTTP methods for Product: PUT, POST, DELETE for READ-ONLY policy.
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        // Disable HTTP methods for ProductCategory: PUT, POST, DELETE for READ-ONLY policy.
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
+
+}
