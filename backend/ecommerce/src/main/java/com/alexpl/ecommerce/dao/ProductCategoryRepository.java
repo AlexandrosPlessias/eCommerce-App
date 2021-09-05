794158b55b029e66f64c33dab7e22b64f6077dec
Alexandros Plessias <67461036+AlexandrosPlessias@users.noreply.github.com>
2021-09-05 19:01:30 +0300

    Update ProductCategoryRepository
    Add CrossOrigin support for ProductCategoryRepository also

--- backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductCategoryRepository.java
+++ backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductCategoryRepository.java
@@ -3,10 +3,12 @@ package com.alexpl.ecommerce.dao;
 import com.alexpl.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // http://localhost:8080/api/product-category
 // "productCategory" is the Name of JSON entry
 // "product-category" is the reference for the path
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
