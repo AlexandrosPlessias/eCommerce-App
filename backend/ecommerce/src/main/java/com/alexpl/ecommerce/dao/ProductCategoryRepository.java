8a67e39ef7fcf552b15ec89c426d2607d5353ba0
Alexandros Plessias <67461036+AlexandrosPlessias@users.noreply.github.com>
2021-09-05 17:20:54 +0300

    Add CORS Support
    Add Cross-Origin Resource Sharing) support for call my repo from "localhost:4200" (By Fron End).

--- backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductCategoryRepository.java
+++ backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductCategoryRepository.java
@@ -4,6 +4,7 @@ import com.alexpl.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// http://localhost:8080/api/product-category
 // "productCategory" is the Name of JSON entry
 // "product-category" is the reference for the path
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
--- backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductRepository.java
+++ backend/ecommerce/src/main/java/com/alexpl/ecommerce/dao/ProductRepository.java
@@ -2,9 +2,21 @@ package com.alexpl.ecommerce.dao;
 
 import com.alexpl.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// http://localhost:8080/api/products
 // "products" is the Name of JSON entry
 // "products" is the reference for the path
+
+// Add CORS (Cross-Origin Resource Sharing) support for call my repo from "localhost:4200" (By Fron End).
+
+// i.e.: An example of a cross-origin request: the front-end JavaScript code served from https://domain-a.com uses XMLHttpRequest
+// to make a request for https://domain-b.com/data.json.
+
+//For security reasons, browsers restrict cross-origin HTTP requests initiated from scripts. For example, XMLHttpRequest and the
+// Fetch API follow the same-origin policy. This means that a web application using those APIs can only request resources from the
+// same origin the application was loaded from unless the response from other origins includes the right CORS headers.
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
