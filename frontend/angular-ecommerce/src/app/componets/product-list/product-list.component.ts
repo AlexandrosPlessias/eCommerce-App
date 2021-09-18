8ae8dd726c29322cefac2d387344b1393725e7c0
Alexandros Plessias <67461036+AlexandrosPlessias@users.noreply.github.com>
2021-09-18 12:47:26 +0300

    Add Routing for categories - static
    Add Routing for left Sidebar

--- frontend/angular-ecommerce/src/app/componets/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/componets/product-list/product-list.component.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
   // Injection of ProductService
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private  route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts()
+    this.route.paramMap
+      .subscribe( () => {
+        this.listProducts()
+
+      }
+    )
   }
 
   listProducts() {
+    // check if 'id' parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      const categoryId = this.route.snapshot.paramMap.get('id');
+      if(categoryId){
+        this.currentCategoryId = parseInt(categoryId);
+      }
+    } else {
+      this.currentCategoryId = 1;
+    }
     this.productService
-      .getProductList()
+      .getProductList(this.currentCategoryId)
       .subscribe(data => {
         this.products = data;
       });
